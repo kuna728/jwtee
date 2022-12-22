d4a5c97946923388bed40a4cf0580b99cdfffefe
kuna7 <kuna728@gmail.com>
2022-12-22 16:34:05 +0100

    tmp

--- jwtee-lib/src/main/java/pl/unak7/jwtee/JWTSessionManager.java
+++ jwtee-lib/src/main/java/pl/unak7/jwtee/JWTSessionManager.java
@@ -1,22 +1,37 @@
 package pl.unak7.jwtee;
 
-import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.fasterxml.jackson.core.type.TypeReference;
 
 import javax.ejb.Local;
-import javax.ejb.Remote;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Map;
-import java.util.Objects;
 
 @Local
-public interface JWTSessionManager {
+public interface JWTSessionManager extends HttpSession {
     Object get(String key, TypeReference typeReference) throws IOException, TokenNotFoundException;
     String getJSON(String key) throws IOException, TokenNotFoundException;
     Map<String, String> getJSONMap() throws IOException, TokenNotFoundException;
     void put(String key, Object value) throws IOException, TokenNotFoundException;
+    default String remove(String key) {
+        return null;
+    }
+
     String getToken() throws TokenNotFoundException;
     void setToken(String token) throws IOException;
+
+    default long getCreationTime() {
+        return 0;
+    }
+    default long getLastAccessedTime() {
+        return 0;
+    }
+    default int getMaxInactiveInterval() {
+        return 0;
+    }
+    default void setMaxInactiveInterval(long maxInactiveInterval) {}
+    default void invalidate() {}
+    default boolean isNew() {
+        return true;
+    }
 }
